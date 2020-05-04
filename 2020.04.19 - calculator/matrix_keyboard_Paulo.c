444942d2945693f9acee03b833edf1d5dbc417f3
Admin368_Dg <58757588+Admin368@users.noreply.github.com>
2020-05-04 11:08:57 +0800

    2020.04.19-Stc_Calculator_v1

--- 2020.04.19 - calculator/matrix_keyboard_Paulo.c
+++ 2020.04.19 - calculator/matrix_keyboard_Paulo.c
@@ -8,16 +8,39 @@ uchar digitx,digity,ans;
 
 
 void getkey();
+void changestate();
 void main();
 {
+uchar state = 0; //Digitx unselected
+uchar digit = 0;
  while(1)
 				{
-					seg(1);
-					dig(1);
+					digitx = 0;
+					while(state == 0;) //state0 Select digitx
+							{
+							 seg(1);	
+							 dig(digitx);
+							 getkey();
+							}
+					while(state == 1;)//state1 select modifier
+							{
+							seg(1)
+							dig(digitx);
+							delay(100);
+							seg(2)
+							}
+					
 				}
 }
 
-
+void changestate()
+	{
+		state = state+1;
+		if (state == 3;) 
+		{
+			state == 0;
+		}
+	}
 void getkey()
 {
 							switch(key)
@@ -25,60 +48,76 @@ void getkey()
 								case 0xee:									
 								  P1 = key;  // light LED
 									digit = 1;
+									changestate();
 									break;
 								case 0xde:									
 								  P1 = key;  // light LED
 									digit = 2;
+									changestate();
 									break;								
 								case 0xbe:									
 								  P1 = key;  // light LED
 								  digit = 3;
+									changestate();
 									break;								
 								case 0x7e:									
 								  P1 = key;  // light LED
-								digit = 4;	
-								break;
+									digit = 4;	
+									changestate();
+									break;
 								case 0xed:									
 								  P1 = key;  // light LED
-								digit = 5;	
-								break;
+									digit = 5;	
+									changestate();
+									break;
 								case 0xdd:									
 								  P1 = key;  // light LED
-								digit = 6;	
-								break;								
+									digit = 6;	
+									changestate();
+									break;								
 								case 0xbd:									
 								  P1 = key;  // light LED
-								digit = 7;	
-								break;								
+									digit = 7;	
+									changestate();
+									break;								
 								case 0x7d:									
 								  P1 = key;  // light LED
-								digit = 8;	
-								break;									
+									digit = 8;	
+									changestate();
+									break;									
 								case 0xeb:									
 								  P1 = key;  // light LED
-								digit = 9;	
-								break;
+									digit = 9;	
+									changestate();
+									break;
 								case 0xdb:									
 								  P1 = key;  // light LED
-								digit = 0;	
-								break;								
+									digit = 0;	
+									changestate();
+									break;								
 								case 0xbb:									
 								  P1 = key;  // light LED
+									changestate();
 									break;								
 								case 0x7b:									
 								  P1 = key;  // light LED
+									changestate();
 									break;
 								case 0xe7:									
 								  P1 = key;  // light LED
+									changestate();
 									break;
 								case 0xd7:									
 								  P1 = key;  // light LED
+									changestate();
 									break;								
 								case 0xb7:									
 								  P1 = key;  // light LED
+									changestate();
 									break;								
 								case 0x77:									
 								  P1 = key;  // light LED
+									changestate();
 									break;								
 								default:
 									P1 = 0xff;// turn off
@@ -86,4 +125,5 @@ void getkey()
 									
 							}
 							return digit;
+							if (state == 0){digitx = digit);
 }
