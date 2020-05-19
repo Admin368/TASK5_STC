66f6509bfc567d006d28671b965b6b5d5c5b75da
Admin368_Dg <58757588+Admin368@users.noreply.github.com>
2020-05-19 11:15:18 +0800

    alott of untested Updates to Calculator_X4
    - addition of clocking
    - development of getdigit
    - starting of developement of the state based display funtion
    - development of  Effector based calculate function

--- 2020.05.11_CALCULATORX_3/stc-calculator.c
+++ 2020.05.11_CALCULATORX_3/stc-calculator.c
@@ -1,8 +1,153 @@
-#include <reg52.h>	
-#include "..\_OwnMadeStcHeaderFiles\paulobetax.c"
+#include <reg52.h>
+
+	
 #define uchar unsigned char
 
-void main(){
-	while(1);
-	xled(1);
+sbit digdef = P2^6;
+sbit segdef = P2^7;
+
+uchar sevenSegData[] = {
+0x3f ,
+0x06 ,
+0x5b ,
+0x4f ,
+0x66 ,
+0x6d ,
+0x7d ,
+0x07 ,
+0x7f ,
+0x6f 
+
+};              
+							 
+uchar columnCheck = 0xf0;
+uchar rowCheck = 0x0f;
+uchar digit;
+uchar digit2;
+uchar ans;
+uchar segValue;
+uchar chooseSeg;
+
+
+uchar temp1, temp2, key;
+
+
+
+uchar flag_o1=0; //flag operand 1
+uchar flag_o2=1; //flag operand 2
+uchar o1,o2,sol;
+
+void delay(uchar x);   // to create time delays
+uchar translatekey(uchar key); 
+void display7seg(uchar  _code);
+ main(){
+    uchar colvalue,rowvalue;
+	while(1){
+		P3 = 0xf0;//columnCheck //11110000
+		if(P3 != 0xf0){
+			delay(10);//verifying button press
+			if(P3 != 0xf0){//columnCheck //11110000
+				colvalue=~0xf0|P3; //flips the 0to1 and vise vesa
+				P3 = 0x0f;//rowCheck //00001111
+				rowvalue = ~0x0f|P3;    // checking row
+				key = colvalue&rowvalue;
+				translatekey(key);		
+ 			}
+		}
+	}
+}
+
+void delay(uchar x)
+{
+	uchar i=0;
+	
+	while(x--)
+	{
+			for(i =0; i<120; i++);
+	}
+	
+}
+
+uchar translatekey(uchar key)
+{
+	switch(key)
+		{
+								case 0xee:									
+								  P1 = key;  // light LED
+									digit = 1;
+									break;
+								case 0xde:									
+								  P1 = key;  // light LED
+									digit = 2;
+									break;								
+								case 0xbe:									
+								  P1 = key;  // light LED
+								  digit = 3;
+									break;								
+								case 0x7e:									
+								  P1 = key;  // light LED
+								digit = 4;	
+								break;
+								case 0xed:									
+								  P1 = key;  // light LED
+								digit = 5;	
+								break;
+								case 0xdd:									
+								  P1 = key;  // light LED
+								digit = 6;	
+								break;								
+								case 0xbd:									
+								  P1 = key;  // light LED
+								digit = 7;	
+								break;								
+								case 0x7d:									
+								  P1 = key;  // light LED
+								digit = 8;	
+								break;									
+								case 0xeb:									
+								  P1 = key;  // light LED
+								digit = 9;	
+								break;
+								case 0xdb:									
+								  P1 = key;  // light LED
+								digit = 0;	
+								break;								
+								case 0xbb:									
+								  P1 = key;  // light LED
+									break;								
+								case 0x7b:									
+								  P1 = key;  // light LED
+									break;
+								case 0xe7:									
+								  P1 = key;  // light LED
+									break;
+								case 0xd7:									
+								  P1 = key;  // light LED
+									break;								
+								case 0xb7:									
+								  P1 = key;  // light LED
+								digit='+';
+									break;								
+								case 0x77:									
+								  P1 = key;  // light LED
+								digit='=';	
+								break;								
+								default:
+									P1 = 0xff;// turn off
+									digit =0;
+									
+		}
+							
+	return digit;
+}
+
+void display7seg(uchar  _code)
+{
+		P0 = 0xfe;
+		segdef = 1;
+		segdef = 0;
+
+		P0 = sevenSegData[_code];
+		digdef = 1;
+		digdef = 0;		
 }
--- 2020.05.15_CALCULATORX_4/stc-calculator.c
+++ 2020.05.15_CALCULATORX_4/stc-calculator.c
@@ -1,8 +1,159 @@
 #include <reg52.h>	
-#include "..\_OwnMadeStcHeaderFiles\paulobetax3.c"
+#include "..\_OwnMadeStcHeaderFiles\paulobetax4.c"
 #define uchar unsigned char
+uchar state = 0;
+uchar inputx = 0;
+uchar inputy = 0;
+uchar effector = 0;
+uchar result = 0;
+uchar changestateto = x;
+uchar clock = 0;
+uchar clocklimit = 10000;
+uchar sleep = 0;
 
+void getdigit();
+void display();
+void changestate();
+void calculate();
 void main(){
-	while(1);
-	xled(1);
+while(1){
+for (clock=0;clock<clocklimit;clock++){
+			display();
+			getdigit();
+			changestate();
+			calculate();
+			if(clock%2000==0){sleep = ~sleep}
+		}	
+	}
 }
+void getdigit(){
+	switch(state)
+	{
+		case 1: // get inputx // operand1
+			xgetfullkey();
+			if (xkey>0){
+				inputx = xkey;
+				changestateto = 2;
+				xresetkey();
+				break;
+				}
+			break;
+		case 2: // get effector
+			xgetfullkey();
+			if (xkey>0){
+				xgeteffector = yes;
+				xgetfullkey();
+				changestateto = 3;
+				xresetkey();
+				break;
+				} 
+			break;			
+		case 3: // get inputy // operand2
+			xgetfullkey();
+			if (xkey>0){
+				inputy = xkey;
+				changestateto = 4;
+				xresetkey();
+				calculate();
+				break;
+				}
+			break;
+		case 4:
+			xgetfullkey();
+			calculate();
+			break();
+		case 4:
+		default:
+			xkey = 0;
+	}
+}
+
+void display(){
+
+	switch(state){
+		case 0:
+			xsegment(11);
+			if(sleep==0){xdisplay(0);}
+			if(sleep==1){xdisplay(n);}
+			break;
+		case 1:
+			if(clocklimit%clock);
+			xsegment(1);
+			if(sleep==0){xdisplay(inputx);}
+			if(sleep==1){xdisplay(n);}
+			xsegment(6);
+			xdisplay(inputy;)
+			break;
+		case 2:
+			if(sleep==0){xdisplay();}
+			if(sleep==1){xdisplay(n);}
+			break;
+		case 3:
+			if(sleep==0){xdisplay();}
+			if(sleep==1){xdisplay(n);}		
+			break;
+		case 4:
+			if(sleep==0){xdisplay();}
+			if(sleep==1){xdisplay(n);}		
+			break;
+		default:
+	}
+	xled(state); // shows state
+}
+
+void changestate(){
+	swicth(changestateto){
+		case 0:
+			state = 0;
+			break;
+		case 1:
+			state = 1;
+			break;
+		case 2:
+			state = 2;
+			break;
+		case 3:
+			state = 3;
+			break;
+		case 4:
+			state = 4;
+			break;
+		default:
+	}
+	changestateto = x;
+}
+
+void calculate(){
+	if(state)
+	switch(effector)
+		case e1: // addition +
+			result = inputx + inputy;
+			break;
+		case e2: // subtraction -
+			result = inputx - inputy;
+			break;
+		case e3: // division /
+			result = inputx / inputy;
+			break;
+		case e4: // multiplication *
+			result = inputx * inputy;
+			break;
+		case e5: // get result
+		default:
+	state = 4;
+	return result;
+}
+
+void idle(){
+
+}
+
+void crash(){}
+
+/*
+state0>selecting inputx
+state1>selecting effector
+state2>selecting inputy
+state3>showing result
+state4>Reserved
+*/
