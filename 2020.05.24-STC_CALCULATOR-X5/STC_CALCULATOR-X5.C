34a14e57dcef26763b98a11d41dee7a766fb38fd
Admin368_Dg <58757588+Admin368@users.noreply.github.com>
2020-05-26 07:33:27 +0800

    Update STC_CALCULATOR-X5.C

--- 2020.05.24-STC_CALCULATOR-X5/STC_CALCULATOR-X5.C
+++ 2020.05.24-STC_CALCULATOR-X5/STC_CALCULATOR-X5.C
@@ -1,2 +1,47 @@
 #include <reg52.h>
-#include <"..\_OwnMadeStcHeaderFiles\paulobetaX5.c">
+#include "..\_OwnMadeStcHeaderFiles\paulobetaX5.c"
+#define uchar unsigned char
+void main(){
+	//unsplit=0;
+	uchar place;
+	uchar lmt;
+	int xi;
+	uchar lastkey = 0;
+	for(place=1;place<=6;place++){
+		split[place] = 0;
+	}	
+while(1){
+	for(place=6;place>=1;place--){
+		//int lastkey = 0;
+	  //for(lmt=1;lmt<=6;lmt++){
+		xgetfullkey();
+		while(xnewkey==6){
+				xgetfullkey();
+				for(xi=1;xi<7;xi++){//display
+					//xsegment(xi);
+					xdisplay(split[xi]);
+					xsegment(xi);
+				}
+			}
+			while(xnewkey==1){
+				//lastkey = xkey;
+				xgetfullkey();
+				for(xi=1;xi<7;xi++){//display
+					//xsegment(xi);
+					//xgetfullkey();
+					xdisplay(split[xi]);
+					xsegment(xi);
+				  //xreset();
+					//split[place] = lastkey;
+					}
+			//unsplit = lastkey;
+			//xspliter(unsplit);
+			//xresetkey();
+				lastkey = xkey;
+				split[place] = lastkey;
+				}
+			//split[place] = lastkey;
+	}
+}
+}
+	
